58ecbe4aaa79984a636c5422e282d37756a96fe4
harudagondi <giogdeasis@gmail.com>
2022-12-03 16:41:10 +0800

    Day 3

---
+++ src/day3.rs
@@ -0,0 +1,83 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+pub fn solve_part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|rucksack| rucksack.split_at(rucksack.len() / 2))
+        .map(|(comp1, comp2)| {
+            (
+                comp1.chars().collect::<HashSet<char>>(),
+                comp2.chars().collect::<HashSet<char>>(),
+            )
+        })
+        .map(|(comp1, comp2)| comp1.intersection(&comp2).copied().collect::<Vec<_>>())
+        .map(|common| common.into_iter().map(priority).sum::<u32>())
+        .sum()
+}
+
+pub fn solve_part2(input: &str) -> u32 {
+    input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(|mut chunk| {
+            (
+                chunk.next().unwrap().chars().collect::<HashSet<char>>(),
+                chunk.next().unwrap().chars().collect::<HashSet<char>>(),
+                chunk.next().unwrap().chars().collect::<HashSet<char>>(),
+            )
+        })
+        .map(|(rs1, rs2, rs3)| {
+            rs1.intersection(&rs2)
+                .copied()
+                .collect::<HashSet<char>>()
+                .intersection(&rs3)
+                .copied()
+                .collect_vec()
+        })
+        .map(|badge| badge.into_iter().map(priority).sum::<u32>())
+        .sum()
+}
+
+fn priority(c: char) -> u32 {
+    match c {
+        'a'..='z' => (c as u32) - 97 + 1,
+        'A'..='Z' => (c as u32) - 65 + 26 + 1,
+        _ => unreachable!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day3::{priority, solve_part1, solve_part2};
+
+    const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    #[test]
+    fn priority_test() {
+        for (c, p) in ('a'..='z').zip(1..=26) {
+            assert_eq!(priority(c), p);
+        }
+
+        for (c, p) in ('A'..='Z').zip(27..=52) {
+            assert_eq!(priority(c), p);
+        }
+    }
+
+    #[test]
+    fn part1() {
+        assert_eq!(solve_part1(INPUT), 157);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(solve_part2(INPUT), 70);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     const DAY1: &str = include_str!("../day1.txt");
     const DAY2: &str = include_str!("../day2.txt");
+    const DAY3: &str = include_str!("../day3.txt");
     print_title("Day 1 - Counting Calories");
     println!("{}", day1::solve_part1(DAY1));
     println!("{}", day1::solve_part2(DAY1));
     print_title("Day 2: Rock Paper Scissors");
     println!("{}", day2::solve_part1(DAY2));
     println!("{}", day2::solve_part2(DAY2));
+    print_title("Day 3: Rucksack Reorganization");
+    println!("{}", day3::solve_part1(DAY3));
+    println!("{}", day3::solve_part2(DAY3));
 }
 
 fn print_title(title: &str) {
